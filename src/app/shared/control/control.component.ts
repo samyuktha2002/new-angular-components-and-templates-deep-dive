a37aeb86d509021c4af1f15e25f1e99759807caa
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-01-11 19:02:22 +0530

    accessing host elemnet programatically

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,20 +7,23 @@ import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  // host: {
-  //   class: 'control',
-  //   '(click)': 'onClick()'
-  // }
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
-  @HostBinding('class') className = 'control';
-  @HostListener('click') onClick() {
-    console.log('Clicked!');
-  }
-  @Input({ required: true }) label!: string;
-
-  // onClick(){
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
   //   console.log('Clicked!');
   // }
+  @Input({ required: true }) label!: string;
+
+  private el = inject(ElementRef);
+
+  onClick(){
+    console.log('Clicked!');
+    console.log(this.el);
+  }
 
 }
