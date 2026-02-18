150734028b769e277cfd0a56f40da3c5f6f93a8e
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-02-18 10:05:33 +0530

    template variables instead of ngModel two way binding

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(){
-    console.log('SUBMITTED');
+  onSubmit(titleElement: HTMLInputElement){
+    const enteredTitle = titleElement.value;
+    console.log('ENTERED TITLE: ' +  enteredTitle);
   }
 }
