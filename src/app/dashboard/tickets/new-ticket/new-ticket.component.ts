71dc436d1d51849fc8bff5d291a2fedc9485919e
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-02-19 10:04:45 +0530

    lifecycle hooks for OnInit and AfterViewInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,9 +10,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(): void {
+    console.log('ON INIT');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
+  }
+
 
   onSubmit(title: string, ticketText:string){
     console.log(title);
